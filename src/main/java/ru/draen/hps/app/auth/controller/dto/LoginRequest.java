75e4197d93869f1f62b729a9ecb00bd598396e18
Draen <haykin.oleg@rambler.ru>
2024-10-13 17:58:38 +0300

    feat: removed auth

--- src/main/java/ru/draen/hps/app/auth/controller/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package ru.draen.hps.app.auth.controller.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginRequest(
-        @NotBlank String username,
-        @NotBlank String password
-) {
-}
--- src/main/java/ru/draen/hps/domain/EUserRole.java
+++
@@ -1,8 +0,0 @@
-package ru.draen.hps.domain;
-
-
-public enum EUserRole {
-    GUEST,
-    // whatever the hell we'll add
-    ADMIN;
-}
